e9a0909525972d22908968e77f23601d914d7298
EvaKarm <ekarmyshova@gmail.com>
2023-06-16 14:20:57 +0300

    Change day format to string and prepare list of items

--- app.js
+++ app.js
@@ -9,40 +9,14 @@ app.set('view engine', 'ejs'); //подключаем ejs, доп. создае�
 app.get("/", function(req, res){
 
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
+   
+    let options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long"
+    };
 
-   switch (currentDay) {
-    case 0:
-        day = "Sunday";
-        break;
-        case 1:
-        day = "Monday";
-        break;
-        case 2:
-        day = "Tuesday";
-        break;
-        case 3:
-        day = "Wednesday";
-        break;
-        case 4:
-        day = "Thursday";
-        break;
-        case 5:
-        day = "Friday";
-        break;
-        case 6:
-        day = "Saturday";
-        break;    
-       default:
-        console.log("Error: current day is equal to: " + currentDay);
-   }
-
-    // if (currentDay === 6 || currentDay === 0) {
-    //     day = "Weekend";
-    //  } else {
-    //     day = "Weekday";
-    // }
+    let day = today.toLocaleDateString('en-US', options);
 
     res.render('list', {kindOfDay: day}); //ejs method, render a file list and find kindOfDay variable and set the value of day
 
